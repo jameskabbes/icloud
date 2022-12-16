eabd4b41b25e5248b91127405656f5c715447b85
James Kabbes <james.kabbes@gmail.com>
2022-12-16 09:33:53 -0600

    restructuring to use Menu

--- src/kabbes_icloud/__main__.py
+++ src/kabbes_icloud/__main__.py
@@ -1,12 +1,9 @@
 import kabbes_icloud
+import py_starter as ps
 
 conn = kabbes_icloud.Connection( account_email = input('Input your Apple account email: '),
-                                 account_password = input('Enter your Apple account password: ')  )
+                                 account_password = ps.get_secret_input('Enter your Apple account password: ')  )
 
 Contacts = kabbes_icloud.ICloudContacts( conn = conn )
+Contacts.run()
 
-selected_Contacts, final_string = Contacts.get_Contacts_from_input()
-selected_Contacts.print_atts()
-
-for Contact in selected_Contacts:
-    Contact.print_all_atts()
